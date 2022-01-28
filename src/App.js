d7f06bda08f3023cf1229b5e9edb3a442aa993d2
Bódogh Péter <ohhaipeter@outlook.com>
2022-01-28 11:59:48 +0200

    Getting There...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { Navbar } from './app/Navbar';
 import { AddPostForm } from './features/posts/AddPostForm';
-
 import { PostsList } from './features/posts/PostsList';
+import { SinglePostPage } from './features/posts/SinglePostPage';
+import { EditPostForm } from './features/posts/EditPostForm';
 
 function App() {
   return (
@@ -27,6 +23,8 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path='/posts/:postId' component={ SinglePostPage } />
+          <Route exact path='/editPost/:postId' component={ EditPostForm } />
           <Redirect to="/" />
         </Switch>
       </div>
